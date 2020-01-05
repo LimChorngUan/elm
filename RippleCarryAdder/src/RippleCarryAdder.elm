f2b5e97798e94f488688d456a64a818e19cc542e
Lim Chorng UanE <limchornguan418@gmail.com>
2020-01-05 16:13:36 +0100

    gosh

---
+++ RippleCarryAdder/src/RippleCarryAdder.elm
@@ -0,0 +1,26 @@
+module RippleCarryAdder exposing (andGate, inverter, orGate)
+
+import Bitwise
+
+
+andGate : Int -> Int -> Int
+andGate a b =
+    Bitwise.and a b
+
+
+orGate : Int -> Int -> Int
+orGate a b =
+    Bitwise.or a b
+
+
+inverter : Int -> Int
+inverter a =
+    case a of
+        0 ->
+            1
+
+        1 ->
+            0
+
+        _ ->
+            -1
